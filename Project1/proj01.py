964442f912073a484a0cf388088d504325a4efc6
Pranesh Vats <90755751+PraneshVats@users.noreply.github.com>
2023-10-24 21:12:01 -0400

    CSE 231 First Project

---
+++ Project1/proj01.py
@@ -0,0 +1,14 @@
+#use the following strings in your input and print statements
+"Input rods: "
+#the following 2 strings should be in the same print statement
+#seperated by the input rods
+"\nYour value is"
+"rods.\n"
+
+"Conversions"
+"Meters:"
+"Feet:"
+"Miles:"
+"Furlongs:"
+"Minutes to walk"
+"rods:
